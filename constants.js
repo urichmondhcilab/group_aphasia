5213206c03809ef16903560f62999d8f8b4333e4
Obiorah <obiorahmmachi@gmail.com>
2026-07-15 19:48:49 -0400

    trying gitignore after git rm

--- constants.js
+++
@@ -1,9 +0,0 @@
-// initializing the publish and subscribe keys for PubNub
-let init_values = {
-  publishKey: "pub-c-931a8a05-bc0b-426f-9d3f-8bee93b34d1f",
-  subscribeKey: "sub-c-e5c423cc-279e-4547-9d1c-321fa2bfd68d",
-};
-
-// // creating a PubNub object
-// const pubnub = new PubNub(init_values);
-
